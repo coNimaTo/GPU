c573dd6d6399a873c06c140cceab3a32ae4a7d54
coNimaTo <nicolstomaselli@yahoo.com.ar>
2026-04-22 13:00:30 -0300

    Saque lo de dividir el HeightMap en .h y .cu xq me parece al dope para tan poco archivo.

--- HydraulicsGPU/src/core/HeightMap.cu
+++ HydraulicsGPU/src/core/HeightMap.cu
@@ -1,23 +1,23 @@
-#include "HeightMap.h"
+#include "utils/CudaUtils.h"   // always use paths relative to src/
 
-__host__ __device__
-float& HeightMap::operator()(int row, int col) {
-    return data[row * size + col];
-}
+struct HeightMap {
+    float *data;
+    int    size;
 
-// Claude dice que es buena practica tener esto para funciones read-only
-__host__ __device__
-const float& HeightMap::operator()(int row, int col) const {
-    return data[row * size + col];
-}
+    __host__ __device__ float& operator()(int row, int col)
+    {return data[row * size + col];}
 
-void HeightMap::allocate(int n) {
+    __host__ __device__ const float& operator()(int row, int col) const
+    {return data[row * size + col];}
+
+    void allocate(int n){
     size = n;
     CUDA_CHECK(cudaMallocManaged(&data, (size_t)n * n * sizeof(float)));
     CUDA_CHECK(cudaMemset(data, 0, (size_t)n * n * sizeof(float)));
-}
+    }
 
-void HeightMap::release() {
+    void release(){
     CUDA_CHECK(cudaFree(data));
     data = nullptr;
-}
+    };
+};
